a491231d39ac3ef8efa3a51eb13c39e4d7b2e6f8
KartaKarta <141367530+EverlongQQQ@users.noreply.github.com>
2025-01-07 14:09:03 +0100

    main.m co nepotrebujeme

--- main.m
+++ main.m
@@ -14,22 +14,6 @@ pocet_trid = round(pocet_trid);
 [tridy_k, stredy_k, J_k] = k_means(data, pocet_trid);
 %%
 [tridy, stredy, J] = nerovnomerne_binarni_deleni(data, pocet_trid);
-%% 3
-iterativni_optimalizace(tridy_k, stredy_k, J_k);
-iterativni_optimalizace(tridy, stredy, J);   
-bayes(tridy, stredy);
-% b)
-minimalni_vzdalenost(tridy, stredy);
-%% c)
-nejblizsi_soused(tridy, stredy);
-%% d)
-rosenblattuv_algoritmus(tridy, stredy);
-konstantni_prirustky(tridy, stredy, 1, 0.2);
-konstantni_prirustky(tridy, stredy, 1, 1);
-konstantni_prirustky(tridy, stredy, 1, 3);
-upravene_konstantni_prirustky(tridy, stredy, 1, 0.2);
-upravene_konstantni_prirustky(tridy, stredy, 1, 1);
-upravene_konstantni_prirustky(tridy, stredy, 1, 3);
 %%
 %ulozeni_figur
 toc
